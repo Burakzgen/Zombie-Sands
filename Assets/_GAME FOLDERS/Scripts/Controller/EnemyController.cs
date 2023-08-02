6864a5bc85b97e0109094932cbeadaa753229fb0
Burak ÖZGEN <35861357+Burakzgen@users.noreply.github.com>
2023-08-02 11:32:50 +0300

    Upgrade nav mesh

--- Assets/_GAME FOLDERS/Scripts/Controller/EnemyController.cs
+++ Assets/_GAME FOLDERS/Scripts/Controller/EnemyController.cs
@@ -1,14 +1,27 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
+    //PRIVATE
     NavMeshAgent _agent;
     Animator _myAnimator;
+    GameManager _gameManager;
+    [Header("TARGET CONTROLS")]
     [SerializeField] Transform _target;
+
+    [Header("OTHER SETTINGS")]
     [SerializeField] private float _health;
     [SerializeField] private float _damage;
-    GameManager _gameManager;
+
+    [Header("DISSOLVE CONTROLS")]
+    [SerializeField] private SkinnedMeshRenderer _skinnedMesh;
+    [SerializeField] private MeshRenderer _headMesh;
+    private Material _dissolveBodyMaterial;
+    private Material _dissolveHeadMaterial;
+    [SerializeField] private float DissolveRate = 0.0125f;
+    [SerializeField] private float RefleshRate = 0.025f;
     private void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
@@ -17,8 +30,22 @@ public class EnemyController : MonoBehaviour
         _gameManager = GameObject.FindWithTag("GameManager").GetComponent<GameManager>();
         //_isDead = false;
 
+        if (_skinnedMesh != null)
+            _dissolveBodyMaterial = _skinnedMesh.material;
+        if (_headMesh != null)
+            _dissolveHeadMaterial = _headMesh.material;
         if (_target != null)
             _agent.SetDestination(_target.transform.position);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Home"))
+        {
+            _gameManager.TakeDamage(_damage);
+            _gameManager.UpdateEnemyCount();
+            gameObject.GetComponent<CharacterController>().enabled = false;
+            Destroy(gameObject, 0.5f);
+        }
     }
     public void SetTarget(Transform target)
     {
@@ -39,18 +66,22 @@ public class EnemyController : MonoBehaviour
         _gameManager.UpdateEnemyCount();
         _myAnimator.Play("Normal_Shot_Dead");
         _agent.speed = 0;
+        StartCoroutine(DissolveCoroutine());
         //TODO: Object pooling kontrolune göre ayarlanacak.
         Destroy(gameObject, 5f);
     }
-    private void OnTriggerEnter(Collider other)
+    private IEnumerator DissolveCoroutine()
     {
-        if (other.gameObject.CompareTag("Home"))
+        float counter = 0;
+        while (_dissolveBodyMaterial.GetFloat("_DissolveAmount") < 1)
         {
-            _gameManager.TakeDamage(_damage);
-            _gameManager.UpdateEnemyCount();
-            gameObject.GetComponent<CharacterController>().enabled = false;
-            Destroy(gameObject, 0.5f);
+            counter += DissolveRate;
+            _dissolveBodyMaterial.SetFloat("_DissolveAmount", counter);
+            _dissolveHeadMaterial.SetFloat("_DissolveAmount", counter);
+            yield return new WaitForSeconds(RefleshRate);
         }
+
+
     }
 
 }
